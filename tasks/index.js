79af7e8e05fc4c1764128b214aa4e49812594a6e
artemkaint <artemkaint@nodasoft.com>
2015-05-19 17:19:09 +0300

    Wrap config in function and return local configured require function. Beautified code

--- tasks/index.js
+++ tasks/index.js
@@ -1,4 +1,3 @@
-var fs = require('fs');
 var prettyStringify = require('js-object-pretty-print').pretty;
 
 module.exports = function (grunt) {
@@ -7,19 +6,24 @@ module.exports = function (grunt) {
   grunt.registerMultiTask('requirejsconfig', 'Prepends a require.js config file.', function () {
 
     var options = this.options();
-
-    if (fs.existsSync(this.data.src)) {
-      var content = [
-        '(function(requirejs) {',
-        '// Config added by grunt-requirejs-config',
-        'return requirejs.config(' + prettyStringify(options, 2, 'JSON', true) + ');',
-        '})(requirejs);',
-        fs.readFileSync(this.data.src, 'utf8')
-      ];
-      fs.appendFileSync(this.data.dest, content.join('\n'));
-      grunt.log.ok('Require.js config created at: ' + this.data.dest);
-    } else {
-      grunt.log.error('No source file found at: ' + this.data.src);
+    var src = '';
+    if (this.data.src) {
+      if (grunt.file.exists(this.data.src)) {
+        src = grunt.file.read(this.data.src, {encoding: 'utf8'});
+      }
+      else {
+        grunt.log.error('No source file found at: ' + this.data.src);
+      }
     }
+
+    var content = [
+      '(function(requirejs) {',
+      '// Config added by grunt-requirejs-config',
+      'return requirejs.config(' + prettyStringify(options, 2, 'JSON', true) + ');',
+      '})(requirejs);',
+      src
+    ];
+    grunt.file.write(this.data.dest, content.join('\n'));
+    grunt.log.ok('Require.js config created at: ' + this.data.dest);
   });
 };
